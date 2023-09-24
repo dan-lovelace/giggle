321b51e17edc4ddc90d9ba6eee4f7c5f3eb10310
dan-lovelace <dlovelace085@gmail.com>
2023-09-24 14:12:28 -0400

    fix: nextjs build

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   experimental: {
     externalDir: true,
   },
