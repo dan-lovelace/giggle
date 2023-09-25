6bba6c7001644e37e12dfdf41b63e26c1589a704
dan-lovelace <dlovelace085@gmail.com>
2023-09-25 19:41:31 -0400

    chore: finish initialize script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
+normal=$'\e[0m'                           # (works better sometimes)
+bold=$(tput bold)                         # make colors bold/bright
+red="$bold$(tput setaf 1)"                # bright red text
+green=$(tput setaf 2)                     # dim green text
+fawn=$(tput setaf 3); beige="$fawn"       # dark yellow text
+yellow="$bold$fawn"                       # bright yellow text
+darkblue=$(tput setaf 4)                  # dim blue text
+blue="$bold$darkblue"                     # bright blue text
+purple=$(tput setaf 5); magenta="$purple" # magenta text
+pink="$bold$purple"                       # bright magenta text
+darkcyan=$(tput setaf 6)                  # dim cyan text
+cyan="$bold$darkcyan"                     # bright cyan text
+gray=$(tput setaf 7)                      # dim white text
+darkgray="$bold"$(tput setaf 0)           # bold black = dark gray text
+white="$bold$gray"                        # bright white text
 
 # install all package dependencies
+echo "Installing dependencies"
 yarn install
 
-# initialize local database
+# # initialize local database
+echo "Creating database and running migrations"
 yarn workspace @giggle/db knex migrate:latest
 
-# create .env with template
+# # create .env with template
+echo "Creating empty ${yellow}.env${normal} file"
 echo "GOOGLE_API_KEY=" >> packages/app/.env
