7c96a3c30acd8d52ffa716432f5033f3de007843
dan-lovelace <dlovelace085@gmail.com>
2023-09-25 20:00:42 -0400

    fix: production build issues

--- packages/db/knexfile.ts
+++ packages/db/knexfile.ts
@@ -3,22 +3,24 @@ import path from "path";
 import { Knex } from "knex";
 
 const cwd = process.cwd();
+const defaultConfig: Knex.Config = {
+  client: "sqlite3",
+  connection: {
+    filename: path.join(
+      cwd,
+      path.basename(cwd) === "db" ? "" : "../db",
+      "db.sqlite3",
+    ),
+  },
+  migrations: {
+    extension: "ts",
+  },
+  useNullAsDefault: true,
+};
 
 const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: path.join(
-        cwd,
-        path.basename(cwd) === "db" ? "" : "../db",
-        "db.sqlite3",
-      ),
-    },
-    migrations: {
-      extension: "ts",
-    },
-    useNullAsDefault: true,
-  },
+  development: defaultConfig,
+  production: defaultConfig,
 };
 
 module.exports = config;
